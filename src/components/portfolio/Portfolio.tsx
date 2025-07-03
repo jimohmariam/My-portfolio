4fca2cc72b405178dcb5e11016203adfbe24cd2b
jimohmariam <155982019+jimohmariam@users.noreply.github.com>
2025-07-03 14:09:56 +0100

    Update Portfolio.tsx

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
       title: "Organic Product eCommerce Website",
       description:
         "A responsive platform where users can purchase organic products. Features include product catalog, shopping cart, user authentication, and payment integration.",
-       image: "https://i.postimg.cc/d0tpvSb6/Screenshot-2025-07-02-at-12-57-14.png",
+       image: "https://i.postimg.cc/hjLdJBdv/Screenshot-2025-07-02-at-12-57-14.png",
       technologies: ["React", "Node.js", "MongoDB", "Express", "Tailwind CSS"],
       status: "Completed",
       highlights: [
